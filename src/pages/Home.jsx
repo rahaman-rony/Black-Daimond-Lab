743303754c04daa60664e44d99c572b576054137
rahaman-rony <ronyvoip88@gmail.com>
2025-05-04 16:31:20 +0600

    Initial setup for Black Diamond Lab

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to Black Diamond Lab</h1>
+      <p>Ethical Hacking | Dark Web Lab | WiFi Tool | Malware Dev | Camera Exploits</p>
+    </div>
+  );
+}
+
+export default Home;
